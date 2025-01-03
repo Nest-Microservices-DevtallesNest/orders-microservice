c5a8113693ad8cab01ca1e3ef0181a0cb1f78f09
Leandro Matus <leandromatus85@gmail.com>
2025-01-03 11:17:20 -0600

    feat: add order management functionalities with DTOs, pagination, and database integration

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,18 +1,82 @@
-import { Injectable } from '@nestjs/common';
+import { OrderPaginationDto } from './dto/order-pagination.dto';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
+import { PrismaClient } from '@prisma/client';
+import { RpcException } from '@nestjs/microservices';
+import { ChangeOrderStatusDto } from './dto/change-order-status.dto';
 
 @Injectable()
-export class OrdersService {
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+export class OrdersService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('OrderService');
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Connected to the database');
   }
 
-  findAll() {
-    return `This action returns all orders`;
+  async create(createOrderDto: CreateOrderDto) {
+    return await this.order.create({
+      data: createOrderDto,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
+  async findAll(orderPaginationDto: OrderPaginationDto) {
+    const totalPages = await this.order.count({
+      where: {
+        status: orderPaginationDto.status,
+      },
+    });
+
+    const currentPage = orderPaginationDto.page;
+    const perPage = orderPaginationDto.limit;
+
+    return {
+      data: await this.order.findMany({
+        where: {
+          status: orderPaginationDto.status,
+        },
+        skip: (currentPage - 1) * perPage,
+        take: perPage,
+      }),
+      meta: {
+        total: totalPages,
+        page: currentPage,
+        last_page: Math.ceil(totalPages / perPage),
+      },
+    };
+  }
+
+  async findOne(id: string) {
+    const order = await this.order.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!order) {
+      throw new RpcException({
+        status: HttpStatus.NOT_FOUND,
+        message: `Order with id ${id} not found`,
+      });
+    }
+    return order;
+  }
+
+  async changeOrderStatus(changeOrderStatusDto: ChangeOrderStatusDto) {
+    const { id, status } = changeOrderStatusDto;
+
+    const order = await this.findOne(id);
+
+    if (order.status === status) {
+      return order;
+    }
+
+    return await this.order.update({
+      where: {
+        id,
+      },
+      data: {
+        status,
+      },
+    });
   }
 }
